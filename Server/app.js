9cdd5a2383b295b304e71dcae07de94fde02dce3
zmzm <vladislav.kovalskij@gmail.com>
2016-04-18 22:46:18 +0300

    bug fix

--- Server/app.js
+++ Server/app.js
@@ -6,9 +6,26 @@ var fs = require('fs');
 var app = express();
 var Shedule = require('./shedule/Shedule');
 
+app.use(express.static(path.join(__dirname, '../Client/app')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(app.router);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../Client/app', 'index.html'));
+});
+
+app.get('/api', function (req, res) {
+    res.send('API is running');
+});
+
+app.get('/api/schedule', function (req, res) {
+    Shedule.getShedule(req.query.date, function (response) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        res.status(200);
+        res.send(response);
+    });
+});
 
 app.use(function (req, res, next) {
     res.status(404);
@@ -23,22 +40,8 @@ app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     log.error('Internal error(%d): %s', res.statusCode, err.message);
     res.send({error: err.message});
-    return;
 });
 
-app.get('/api', function (req, res) {
-    res.send('API is running');
-});
-
-app.get('/api/schedule', function (req, res) {
-
-    Shedule.getShedule(req.query.date, function (response) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        res.status(200);
-        res.send(response);
-    });
-});
 
 app.listen(3000, function () {
     log.info('Express server listening on port 3000');
--- Server/request/SheduleRequest.js
+++ Server/request/SheduleRequest.js
@@ -2,8 +2,9 @@ var request = require('request');
 
 
 module.exports.ScheduleRequest = function (param, callback) {
+    //arg0 - номер группы, arg1 - номер факультета, arg2 - форма обучения, agr3 - курс, agr4 - хз что, date - дата
     request({
-        uri: "http://raspisanie.grsu.by/TimeTable/PrintPage.aspx?arg0=960&arg1=3&arg2=2&arg3=4%20&arg4=1&date=" + param + "%200:00:00",
+        uri: "http://raspisanie.grsu.by/TimeTable/PrintPage.aspx?arg0=960&arg1=3&arg2=2&arg3=5%20&arg4=1&date=" + param + "%200:00:00",
         method: "GET",
         timeout: 10000,
         encoding: "utf-8",
